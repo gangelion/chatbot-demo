0f45ce1416504079eef00c2b9b94ec8271e8d7cd
tetsuya.kojima <tetsuya.kojima@fox-hound.tech>
2020-10-24 15:58:08 +0900

    material-uiの導入, componentsの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import defaultDataset from "./dataset";
-import './assets/styles/style.css'
+import './assets/styles/style.css';
+import {AnswersList} from './components/index';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            answeres: [],
+            answers: [],
             chats: [],
             currentId: "init",
             dataset: defaultDataset,
             open: false,
         };
     }
+
+    initAnswer = () => {
+        const initDataset = this.state.dataset[this.state.currentId];
+        const initAnswer = initDataset.answers;
+
+        this.setState({
+            answers: initAnswer
+        })
+    }
+
+    componentDidMount() {
+        this.initAnswer()
+    }
     render() {
         return (
             <section className="c-section">
                 <div className="c-box">
+                    <AnswersList answers={this.state.answers}/>
                 </div>
             </section>
         );
